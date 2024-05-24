9ac00d874ec35b9620535e3abac6d0ea2e6b286d
iamKing <king@gmail.com>
2024-05-24 11:27:30 +0000

    added style to text

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+
+    <div>
+
+      <p class="text-red-500">hello world</p>
+    </div>
+  </header>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p clas="text-red">The only boss with one head</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
